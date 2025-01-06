c94055ff688f95ed77fa01b3acc8b83ad7ac5947
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-06 14:40:57 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React from "react";
 import "./App.css";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 const App = () => {
   return (
     <div className="container">
+      <div className="brand-box">
+        <h1>Magic Form</h1>
+        <p>Build forms in React without the tears.</p>
+      </div>
 
+      <div className="magic-form">
+        <Formik
+          initialValues={{
+            name: "",
+            email: "",
+            agree: false,
+            favoriteColor: "",
+          }}
+          validationSchema={Yup.object({
+            name: Yup.string().required("Isim bos birakilamaz"),
+            email: Yup.string().email().required("Email bos birakilamaz"),
+            agree: Yup.boolean().required("Kosullari kabul etmelisiniz"),
+            favoriteColor: Yup.string()
+              .required("Hadi ama herkesin sevdigi bir renk vardir!")
+              .oneOf(["red", "blue", "green"]),
+          })}
+        >
+          {(props) => (
+            <form>
+              <h3>Kaydol</h3>
+              <label htmlFor="name">Isim</label>
+              <input
+                id="name"
+                type="text"
+                placeholder="Mauro"
+                className="input"
+                value={{}}
+                onChange={{}}
+              />
+            </form>
+          )}
+        </Formik>
+      </div>
     </div>
-  )
+  );
 };
 
 export default App;
