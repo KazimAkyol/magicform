19e40b954055f2df828355a755a9767b28d27526
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-06 15:05:19 +0100

    fourth commit

--- src/App.js
+++ src/App.js
@@ -28,18 +28,70 @@ const App = () => {
               .oneOf(["red", "blue", "green"]),
           })}
         >
-          {(props) => (
+          {({
+            values,
+            errors,
+            handleChange,
+            handleSubmit,
+            handleReset,
+            dirty,
+            isSubmitting,
+          }) => (
             <form>
-              <h3>Kaydol</h3>
-              <label htmlFor="name">Isim</label>
+              <h3>Register</h3>
+              <label htmlFor="name">Name:</label>
               <input
                 id="name"
                 type="text"
-                placeholder="Mauro"
+                placeholder="Mauro..."
                 className="input"
-                value={{}}
-                onChange={{}}
+                value={values.name}
+                onChange={handleChange}
               />
+              <label htmlFor="email" className="topMargin">
+                Email:
+              </label>
+              <input
+                id="email"
+                type="email"
+                placeholder="icardimauro@gmail.com"
+                className="input"
+                value={values.email}
+                onChange={handleChange}
+              />
+              <label htmlFor="favoriteColor" className="topMargin">
+                Favorite Color
+              </label>
+              <select
+                name=""
+                id="favoriteColor"
+                value={values.favoriteColor}
+                onChange={handleChange}
+                style={{
+                  marginTop: 10,
+                  width: "150px",
+                  padding: "10px, 15px",
+                  outline: "none",
+                }}
+              >
+                <option value="" label="Choice Color" />
+                <option value="red" label="Red" />
+                <option value="blue" label="Blue" />
+                <option value="green" label="Green" />
+              </select>
+              <div className="checkbox topMargin">
+                <input
+                  id="agree"
+                  type="checkbox"
+                  value={values.agree}
+                  onChange={handleChange}
+                />
+                <label htmlFor="agree" className="checkbox-label">
+                  I have read and accept the contract.
+                </label>
+              </div>
+              <button type="submit" disabled={!dirty || isSubmitting}></button>
+              Register
             </form>
           )}
         </Formik>
